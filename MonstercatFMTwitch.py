b3d0a64a97d6c798ef5fb9e263b8b7dfeff65919
Maximilian Berger <snowdragon92@gmail.com>
2015-04-19 03:01:47 +0200

    Autoformat with pep8 autoformatter

--- MonstercatFMTwitch.py
+++ MonstercatFMTwitch.py
@@ -1,9 +1,9 @@
 import socket
-import string
 import getpass
 import os
 import os.path
 import re
+
 
 def header():
     print("       __  ___                 __                       __      ________  ___   ", end='')
@@ -16,9 +16,11 @@ def header():
     print("Source available on GitHub            [http://github.com/thinkaliker/MCFMPython]", end='')
     print("////////////////////////////////////////////////////////////////////////////////")
 
+
 def belowline():
     print("Songs will appear below.")
     print("================================================================================")
+
 
 def styleprompt():
     print("Text output styles")
@@ -28,26 +30,29 @@ def styleprompt():
     print(" [3]  Artist - Song")
     print(" [4]  Song - Artist")
 
-def fileprompt():
-    print("Enter your text file output location (Somewhere on C:\ recommended)")
 
-song=""
-artist=""
+def fileprompt():
+    print(
+        "Enter your text file output location (Somewhere on C:\ recommended)")
+
+song = ""
+artist = ""
 
 header()
 
+
 def styleSwitch(style):
-  styles = {
-    1 : (" " + artist + " // " + song + " "),
-    2 : (" " + song + " // " + artist + " "),
-    3 : (" " + artist + " - " + song + " "),
-    4 : (" " + song + " - " + artist + " "),
-    5 : (" " + artist.upper() + " // " + song.upper() + " "),
-    6 : (" " + song.upper() + " // " + artist.upper() + " "),
-    7 : (" " + artist.upper() + " - " + song.upper() + " "),
-    8 : (" " + song.upper() + " - " + artist.upper() + " ")
-  }
-  return styles[style]
+    styles = {
+        1: (" " + artist + " // " + song + " "),
+        2: (" " + song + " // " + artist + " "),
+        3: (" " + artist + " - " + song + " "),
+        4: (" " + song + " - " + artist + " "),
+        5: (" " + artist.upper() + " // " + song.upper() + " "),
+        6: (" " + song.upper() + " // " + artist.upper() + " "),
+        7: (" " + artist.upper() + " - " + song.upper() + " "),
+        8: (" " + song.upper() + " - " + artist.upper() + " ")
+    }
+    return styles[style]
 
 fileprompt()
 fileloc = input("File path: ")
@@ -56,34 +61,35 @@ if (os.path.isfile(fileloc)) != 1:
     print("File does not exist. Creating.")
     open(fileloc, 'w+')
 
-NICK=input("Enter your username: ")
+NICK = input("Enter your username: ")
 
-CHANNEL=input("Enter the channnel you would like to join (eg. Monstercat): ")
-CHANNEL="#" + CHANNEL
+CHANNEL = input("Enter the channnel you would like to join (eg. Monstercat): ")
+CHANNEL = "#" + CHANNEL
 
 print("Right click and paste your oauth key beginning with \"oath:\""),
-PASSWORD=getpass.getpass('(hidden):')
+PASSWORD = getpass.getpass('(hidden):')
 
+HOST = "irc.twitch.tv"
+PORT = 6667
+IDENT = NICK
+REALNAME = NICK
 
-HOST="irc.twitch.tv"
-PORT=6667
-IDENT=NICK
-REALNAME=NICK
+readbuffer = ""
 
-readbuffer=""
 
 def sendIRC(stuff, t):
     t.send(stuff.encode('utf-8'))
 
 print("Connecting...")
-s=socket.socket( )
+s = socket.socket()
 s.connect((HOST, PORT))
 sendIRC("PASS %s\r\n" % PASSWORD, s)
 sendIRC("NICK %s\r\n" % NICK, s)
 sendIRC("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME), s)
 sendIRC("JOIN %s\r\n" % CHANNEL, s)
 
-substring=":monstercat!monstercat@monstercat.tmi.twitch.tv PRIVMSG " +CHANNEL+ " :Now Playing:"
+substring = ":monstercat!monstercat@monstercat.tmi.twitch.tv PRIVMSG " + \
+    CHANNEL + " :Now Playing:"
 
 styleprompt()
 
@@ -96,23 +102,24 @@ if caps == "y":
 songartist = styleSwitch(style)
 belowline()
 sendIRC("PRIVMSG " + CHANNEL + " :!song\r\n", s)
-currentsongartist=songartist
+currentsongartist = songartist
 init = 1
 
 while 1:
-    readbuffer=(s.recv(1024)).decode("utf-8")
+    readbuffer = (s.recv(1024)).decode("utf-8")
     if readbuffer.find(substring) != -1:
-        song, artist=re.search('Now Playing: (.*) by (.*) - Listen', readbuffer).groups()
+        song, artist = re.search(
+            'Now Playing: (.*) by (.*) - Listen', readbuffer).groups()
 
-        songartist=styleSwitch(style)
+        songartist = styleSwitch(style)
 
         if (init or currentsongartist != songartist):
             init = 0
 
             print(songartist)
-            f=open(fileloc, 'r+')
+            f = open(fileloc, 'r+')
             f.seek(0)
             f.write(songartist)
             f.truncate()
             f.close()
-            currentsongartist=songartist            
+            currentsongartist = songartist
